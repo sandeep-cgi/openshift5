141753e96dc8249b3dff3b4fbd86fd85c557826d
Ramesh SS KHARAT <ramesh@priarworld.com>
2022-03-22 02:00:21 +0530

    1

--- createtables.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE priarworld ( firstname varchar(20), rollnumber int(5));
-CREATE TABLE priarworld1 ( firstname varchar(20), rollnumber int(5));
--- insertvalues.sql
+++ insertvalues.sql
@@ -3,3 +3,6 @@ INSERT INTO priarworld (firstname, rollnumber) VALUES ('Nayan',2);
 INSERT INTO priarworld (firstname, rollnumber) VALUES ('Chait',3);
 INSERT INTO priarworld (firstname, rollnumber) VALUES ('Rutuja',4);
 INSERT INTO priarworld (firstname, rollnumber) VALUES ('Tanmay',5);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('B1',1003);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('B2',1104);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('B3',1005);
